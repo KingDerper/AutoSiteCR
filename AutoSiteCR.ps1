f127210c5ce32091209b66cb6502952ae8c1e333
KingDerper <57719036+KingDerper@users.noreply.github.com>
2019-11-13 15:28:11 +0000

    These are the core components.

---
+++ AutoSiteCR.ps1
@@ -0,0 +1,52 @@
+﻿<#
+.Synopsis
+THis builds sites and fretches data From a SP list.
+
+.Description
+
+This Module uses a some other modules to build SP site Collections. 
+
+.Example:
+
+AutoSiteCR, this will call in  NEW-FBDSiteAUTO So see that for details
+
+#> 
+Function AutoSiteCR{
+if((Get-PSSnapin | Where {$_.Name -eq "Microsoft.SharePoint.PowerShell"}) -eq $null) {
+ Add-PSSnapin Microsoft.SharePoint.PowerShell;
+}
+$NewSiteURL = "https://Portal.Domain.com"
+$sourceWebURL = "Targetsite"
+$sourceListName = "NewRequests"
+
+$spSourceWeb = Get-SPWeb $sourceWebURL
+$spSourceList = $spSourceWeb.Lists[$sourceListName]
+$spSourceItems = $spSourceList.Items | where {$_['Request Status'] -eq "Approved - Awaiting site creation"}
+$list = $spSourceWeb.Lists[$sourceListName]
+$spSourceItems | ForEach-Object {
+ $Title = $_['Please provide the title for your SharePoint Team Space:']
+ $url = $_['Does your team have an abreviated name or acronym? If so, please provide this:']
+ $USERS= $_['Please provide the names of the Business Owners for your SharePoint Space:']
+ Write-Host "Create site: $TITLE"
+ NEW-FBDSiteAUTO -ShortName $url -Site $title  #–url $url -name $Title -template teamspaces#0 
+ $_['Request Status'] = "Site Created - Request completed"
+ #$_['URL'] = $url + ", " + $Title 
+         foreach($user in $USERS)
+           {
+               Try{
+                 #Get the Group and User
+                   $grp=get-spsite -Limit all |?{$_.url -like "*$URL*"}
+                    $group=$grp.rootweb.SiteGroups|?{$_ -like "*OWNER*"}
+                    $group.AddUser($user.User)
+                    #Add user to Group
+    
+                    Write-Host "$($User) Added to $group Successfully!" -ForegroundColor Green
+                 }
+            Catch{
+                Write-Host "there was an error!"
+                $Error
+                  }
+        }
+ $_.Update()
+}
+}
---
+++ New-FBDSITEAUTO.psm1
@@ -0,0 +1,133 @@
+﻿<#
+.Synopsis
+It will build sites for us.
+
+.Description
+
+This is a module for the creation of a new team site, with the current template, quotas, features and sharepoint groups and permissions. 
+
+.Example:
+
+New-FBDsite -Shortname (the short or abbraviated Name of the Site) -Site (The Title Of the Site) everything else is called in.
+
+#> 
+Function NEW-FBDSiteAUTO(){
+    
+    
+    [CmdletBinding()]
+    Param(
+    [Parameter (mandatory=$true, Position = 0, Helpmessage="The Short name for the Site/abbreviation")]
+    [String]$ShortName,
+    [Parameter (mandatory=$true, Position = 1, Helpmessage="The Full name for the Site to be give as the Site title")]
+    [String]$Site,
+    [Parameter(mandatory=$False, Position = 2, Helpmessage="Do you require a new DB with this site")]
+    [Switch]$FBDConTDB,
+    [Parameter (mandatory=$False, Position = 3, Helpmessage="OwnerAlias Display Name")]
+    [String]$Primary,
+    [Parameter (mandatory=$False, Position = 4, Helpmessage="SecondaryOwnerAlias DisplayName")]
+    [String]$Secondary,
+    [Parameter (mandatory=$False, Position = 5, Helpmessage="SecondaryOwnerAlias DisplayName")]
+    [String]$DefaultGrps
+    )
+
+    sleep 5
+    $assignmentCollection = Start-SPAssignment;
+    if ($FBDConTDB){
+    FBDConTDB
+    
+    }
+    If(!$primary){
+    $PUSER=Get-SPManagedAccount|select username|?{$_.username -like "*Config*"}
+    $primary=$PUSER.Username.Replace('Domainname\','')
+    }
+    Write-host "Primary User is going to be"$primary
+    
+    if (!$secondary){
+    $secondary = $env:USERNAME
+    Write-Host "Secondary Owner has been Set as you!"$Secondary
+    }
+    <#If(!$FBDConTDB)
+        {
+        Write-host "Would you want to create a DB (Default is No)" -ForegroundColor Yellow 
+        $Readhost = Read-Host " ( y / n ) " 
+        Switch ($ReadHost) 
+            { 
+             Y {Write-host "Yes, DB will be made" -ForegroundColor Green; FBDConTDB} 
+             N {Write-Host "No, DB will not be Made" -ForegroundColor Green; $FBDConTDB=$false} 
+             Default {Write-Host "Default, Skip DB Creation"; $FBDConTDB=$false} 
+            }
+    
+        }#>
+        Try{
+             write-host "Getting Portal WebApp" -ForegroundColor Green
+             $WEBAPP=Get-SPWebApplication |?{$_.Displayname -like "portal"} 
+             $WA=$WEBAPP.DisplayName
+             Write-Host $WA -ForegroundColor Yellow
+             $URL=$WEBAPP.Url
+             $Managedpath=Get-SPManagedPath -WebApplication $URL|?{$_.name -like "sites"}
+             $path=$managedpath.Name
+             $siteURL="$url$Path/$Shortname"
+             New-SPSite -url $url$Path/$Shortname -Name "$site" -OwnerAlias "Domain\$primary" -SecondaryOwnerAlias "Domain\$secondary" -template "TeamSpaces#0" -QuotaTemplate Teams –AssignmentCollection $assignmentCollection;
+             Write-host "Site built" -foregroundcolor Green
+            sleep 5
+            Write-Host "Creating Groups"
+            DefaultGrps
+            Stop-SPAssignment $assignmentCollection;
+            }
+        Catch{
+                Write-host "An Error Occured, Below Will say where the Error occured" -BackgroundColor Red -ForegroundColor Green
+                Write-Host $_.scriptStackTrace
+                Write-Host $_.exception.message
+             }
+        Stop-SPAssignment $assignmentCollection;
+        write-host "Clean up will begin, Ignore all 'Clear-Variable :' type errors" -ForegroundColor Green
+        try{
+        Clear-Variable data, WA, WEBAPP, Managedpath, path, primary, secondary, shortname , assignmentCollection, Assign
+        }
+        Catch {
+        Write-host "An Error Occured, Below Will say where the Error occured" -BackgroundColor Red -ForegroundColor Green
+            Write-Host $_.scriptStackTrace
+            Write-Host $_.exception.message
+                }
+       
+        }
+    
+    Function DefaultGrps(){
+        $assign=Start-SPAssignment; 
+        $WEB= Get-SPSite $siteURL -AssignmentCollection $assign
+        $RootWeb=$WEB.rootweb
+        $groupowner=$web.Owner.UserLogin
+        $RootWeb.CreateDefaultAssociatedGroups($GroupOwner, "", $RootWeb.title)
+        sleep 5
+        Enable-SPFeature -Identity da329259-c931-423f-b149-2396bc13a2de -URL $siteURL
+        new-spuser 'Domain\Groupaccount' -Web $web.url -SiteCollectionAdmin -AssignmentCollection $assign
+        Stop-SPAssignment $assign;                   } 
+                                         
+    Function FBDConTDB(){
+    $data=Get-SPDatabase|select server|?{$_.Server -like "spData"}|select -First 1
+    $REdata=$data.Server
+    Write-host "This is the SQL instance the new Database will be added to:" $REdata  -ForegroundColor Yellow
+    Write-host "Please Confirm the above is correct, If incorrect Press CRTL C" -foregroundcolor RED
+    pause
+    write-host "Getting Portal WebApp" -ForegroundColor Green
+    $WEBAPP=Get-SPWebApplication |?{$_.Displayname -like "portal"} 
+    $WA=$WEBAPP.DisplayName
+    $URL=$WEBAPP.Url
+    Write-Host $WEBAPP -ForegroundColor Yellow
+    Write-Host "Is this Correct" -ForegroundColor Red
+    Pause
+    Write-host "new DB Name will be 'BS_PROD_Portal_$ShortName'" -ForegroundColor DarkGreen
+        Try{
+        
+        New-SPContentDatabase “BS_PROD_Portal_$ShortName" -DatabaseServer $REdata -WebApplication $WA  –AssignmentCollection $assignmentCollection 
+        write-host "content Built" -foregroundcolor Green
+        }
+            Catch{
+            Write-host "An Error Occured, Below Will say where the Error occured" -BackgroundColor Red -ForegroundColor Green
+            Write-Host $_.scriptStackTrace
+            Write-Host $_.exception.message
+            
+            }
+    Sleep 5
+    }
+    
